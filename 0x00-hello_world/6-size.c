ed872a377057a3dbc601234e9f07ef6c2db2a91d
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-06-30 14:44:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-retunr(0);
+return (0);
 }
