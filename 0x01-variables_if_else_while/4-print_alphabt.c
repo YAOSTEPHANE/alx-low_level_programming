6cdacce45148f8b6703e3aac35ec149c63e92fb2
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-07-01 17:29:02 -0700

    4-print_alphabt.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,8 @@
 int main(void)
 {
 	int i = 97;
-	while (i < 123);
+
+	while (i < 123)
 	{
 		if (i != 101 && i != 113)
 		{
@@ -20,5 +21,6 @@ int main(void)
 		i++;
 	}
 	putchar(10);
+
 	return (0);
 }
