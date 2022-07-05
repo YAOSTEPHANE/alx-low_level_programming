2a9bd1977bf3789d44097472028f2f54774fa266
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-07-05 09:13:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <holberton.h>
+#include <main.h>
 
 /**
- * main-"The program prints _putchar followed by a new line"
- * File: "0-putchar.c"
- * file: "main.h"
- * Return: Always 0
+ * main-prints main.h
+ * Description: prints main
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[10] = "main";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
