7a376b0991edee1f072db73f0127fee5fe2d5cd7
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-07-06 16:39:29 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,11 +8,6 @@
  * Return: largest number
  */
 
-largest = largest_number(a, b, c);
-
-printf("%d is the largest number\n", largest);
-return (0);
-}
 int largest_number(int a, int b, int c)
 {
 int largest;
