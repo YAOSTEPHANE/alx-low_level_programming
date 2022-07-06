c213f2a74e93b39ba0ddb7bb422f5ff09d445f6b
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-07-06 11:53:02 -0700

    2 task

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,23 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
-int main(void)
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
 {
-        int a, b, c;
-        int largest;
 
-        a = 972;
-        b = -98;
-        c = 0;
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
 
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
 }
