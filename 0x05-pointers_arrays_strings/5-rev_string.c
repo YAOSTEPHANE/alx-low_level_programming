65c59e3847422ea5e4bbf145caebca6bdeeaaf5c
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-07-11 02:17:37 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,24 +8,21 @@
 
 void rev_string(char *s)
 {
-	int a, len;
+	"steph"->"yao"
+	char tmp;
+	int i, length1, length2;
 
-	char *begin, *end = s;
+	length1 = 0;
+	length2 = 0;
 
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
-	{
-		end++;
-	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
-	{
-		char x;
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
-	}
-	end[len + 1] = '\0';
+	while (s[length1] != '\0')
+		length1++;
+length2 = length1 - 1;
+for (i = 0; i < length1 / 2 ; i++)
+{
+tmp = s[i];
+s[i] = s[length2];
+s[length2] = tmp;
+length2 -= 1;
+}
 }
