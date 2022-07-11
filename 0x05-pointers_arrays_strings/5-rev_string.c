9151d778dfc11bfe1a8d635d4cf98ed6e480e982
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-07-11 01:47:37 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * rev_string - This fuction print a string reverser
@@ -21,7 +21,6 @@ void rev_string(char *s)
 	for (a = 0; a < len / 2; a++)
 	{
 		char x;
-
 		x = *end;
 		*end = *begin;
 		*begin = x;
