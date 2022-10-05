ef0b2abfacb17fdab8535bcd9e7964c8a64e3c86
YAOSTEPHANE <kouassistephane489@gmail.com>
2022-10-05 09:16:27 -0700

    all tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/baccrie/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget https://github.com/YAOSTEPHANE/alx-low_level_programming/raw/master/0x18-dynamic_libraries/makemewin.so -O /tmp/makemewin.so
+export LD_PRELOAD=/tmp/makemewin.so
