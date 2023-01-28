8009727f02f4cbf50d534f733c32fd81521df7b3
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2023-01-28 08:58:12 +0000

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @size: The length of the array.
  * @value: The value to look for.
  *
- * Return: The first index of the value in the array, otherwise 1.
+ * Return: The first index of the value in the array, otherwise -1.
  */
 int linear_search(int *array, size_t size, int value)
 {
